71a4426e62049470453e089566bf06cb630fa41e
Matvey-Makaro <matvey.makaro@gmail.com>
2022-04-26 23:01:23 +0300

    Add conversion from function to dict. Add conversion from dict to primitives and function. Serialization of modules doesn't work.

--- Serializer.py
+++ Serializer.py
@@ -8,7 +8,6 @@ IMPORTANT_FUNCTION_ATTRIBUTES = [
     "__closure__"
 ]
 
-
 CODE_ATTRIBUTES = [
     "co_argcount",
     "co_posonlyargcount",
@@ -28,10 +27,9 @@ CODE_ATTRIBUTES = [
     "co_cellvars"
 ]
 
-
 CODE_FIELD_NAME = "__code__"
 GLOBALS_FIELD_NAME = "__globals__"
-DEFAULTS_FIELD_NAME = "__default__"
+DEFAULTS_FIELD_NAME = "__defaults__"
 CLOSURE_FIELD_NAME = "__closure__"
 CODE_NAMES_FIELD_NAME = "co_names"
 
@@ -51,6 +49,7 @@ def dict_to_dict(dictionary: dict) -> dict:
     result = {}
     for key, value in dictionary.items():
         result[key] = obj_to_intermediate_format(value)
+    # return {'dict': result}
     return result
 
 
@@ -76,10 +75,10 @@ def function_to_dict(function) -> dict:
                 if co_name == function.__name__:
                     result[GLOBALS_FIELD_NAME][co_name] = function.__name__
                     continue
-                if co_name in glob and not inspect.ismodule(glob[co_name]) and not inspect.isbuiltin(glob[co_name]):
+                if co_name in glob:  # and not inspect.ismodule(glob[co_name]) and not inspect.isbuiltin(glob[co_name])
                     print(f"Debug: {glob[co_name]}")
                     result[GLOBALS_FIELD_NAME][co_name] = obj_to_intermediate_format(glob[co_name])
-
+    result = {"function": result}
     return result
 
 
@@ -99,5 +98,73 @@ def obj_to_intermediate_format(obj) -> dict or int or float or complex or bool o
         return function_to_dict(obj)
     if inspect.isclass(obj):
         pass
+    # if inspect.ismodule(obj):
+    #     return obj
     else:
         pass
+
+
+def format_to_tuple(form: tuple) -> tuple:
+    result = []
+    for element in form:
+        result.append(intermediate_format_to_obj(element))
+    return tuple(result)
+
+
+def format_to_list(form: list) -> list:
+    result = []
+    for element in form:
+        result.append(intermediate_format_to_obj(element))
+    return result
+
+
+def format_to_dict(form: dict) -> dict:
+    result = {}
+    for key, value in form.items():
+        result[key] = intermediate_format_to_obj(value)
+    return result
+
+
+def form_to_code(form):
+    return CodeType(
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[0]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[1]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[2]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[3]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[4]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[5]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[6]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[7]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[8]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[9]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[10]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[11]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[12]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[13]]),
+        intermediate_format_to_obj(form[CODE_ATTRIBUTES[14]]), intermediate_format_to_obj(form[CODE_ATTRIBUTES[15]]))
+
+
+def format_to_function(form):
+    for key, value in form.items():
+        if key == CODE_FIELD_NAME:
+            code = form_to_code(value)
+        elif key == GLOBALS_FIELD_NAME:
+            glob = value
+        elif key == DEFAULTS_FIELD_NAME:
+            default = value
+        elif key == CLOSURE_FIELD_NAME:
+            closure = value
+        else:
+            raise ValueError("Incorrect key value in function format")
+    return FunctionType(code, glob, None, default, closure)
+
+
+def intermediate_format_to_obj(form: dict or int or float or complex or bool or str or bytes or None):
+    if isinstance(form, dict):
+        for key, value in form.items():
+            if key == 'tuple':
+                return format_to_tuple(value)
+            if key == 'list':
+                return format_to_list(value)
+            if key == 'function':
+                return format_to_function(value)
+            else:
+                return format_to_dict(form)
+
+    if isinstance(form, (int, float, complex, bool, str, bytes)):
+        return form
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import inspect
 from types import *
 import Serializer
+import parser
 
 import math
 c = 42
@@ -16,10 +17,36 @@ def f(x):
 def main() -> None:
     global a
     a = 3
+    dict = {1: "one", 2: "Two"}
     t = (1, 5, 7, 10)
-    l = [1, 2, t, 4, 5]
-    print(Serializer.obj_to_intermediate_format(f))
+    l = [1, 2, t, dict, 5]
+    print(f"Function {f(1)}")
+    dict = {"1": t, "2": l, "3": 3}
+    tmp = Serializer.obj_to_intermediate_format(f)
+    print(tmp)
+    # result = parser.parse(str(tmp))
+    result = eval(str(tmp))
+    print(type(result))
+    print(result)
+    new_func = Serializer.intermediate_format_to_obj(result)
+    # print(f"Function {new_func(1)}")
+    # print(inspect.getmembers(math))
+    # print(type(math))
+    # print(math)
+    # print(math.__dict__)
+    # print(math.__name__)
+    help(ModuleType)
+    modul = ModuleType("math")
+    modul.__loader__ = math.__loader__
 
+
+    print(inspect.getmembers(modul))
+    print(inspect.getmembers(math))
+    print(math.__dir__())
+    print(math.__doc__)
+    print(f.__globals__)
+    print(f.__code__.co_names)
+    # print(f.__globals__('math').)
 
     print("--------------------------------------------------------")
     # for attr in CODE_ATTRIBUTES:
