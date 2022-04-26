594e963eaee608972bb4dfd71d5ba696dc9ba48b
Matvey-Makaro <matvey.makaro@gmail.com>
2022-04-26 16:42:50 +0300

    Add conversion from primitives and function to dict.

---
+++ Serializer.py
@@ -0,0 +1,103 @@
+import inspect
+from types import *
+
+IMPORTANT_FUNCTION_ATTRIBUTES = [
+    "__code__",
+    "__globals__",
+    "__defaults__",
+    "__closure__"
+]
+
+
+CODE_ATTRIBUTES = [
+    "co_argcount",
+    "co_posonlyargcount",
+    "co_kwonlyargcount",
+    "co_nlocals",
+    "co_stacksize",
+    "co_flags",
+    "co_code",
+    "co_consts",
+    "co_names",
+    "co_varnames",
+    "co_filename",
+    "co_name",
+    "co_firstlineno",
+    "co_lnotab",
+    "co_freevars",
+    "co_cellvars"
+]
+
+
+CODE_FIELD_NAME = "__code__"
+GLOBALS_FIELD_NAME = "__globals__"
+DEFAULTS_FIELD_NAME = "__default__"
+CLOSURE_FIELD_NAME = "__closure__"
+CODE_NAMES_FIELD_NAME = "co_names"
+
+
+def sequence_to_dict(sequence: list or tuple) -> dict:
+    result_list = []
+    for s in sequence:
+        result_list.append(obj_to_intermediate_format(s))
+    if isinstance(sequence, list):
+        return {'list': result_list}
+    if isinstance(sequence, tuple):
+        return {'tuple': result_list}
+    raise ValueError("Value error in sequence_to_dict")
+
+
+def dict_to_dict(dictionary: dict) -> dict:
+    result = {}
+    for key, value in dictionary.items():
+        result[key] = obj_to_intermediate_format(value)
+    return result
+
+
+def code_to_dict(code) -> dict:
+    result = {}
+    for attr in CODE_ATTRIBUTES:
+        result[attr] = obj_to_intermediate_format(code.__getattribute__(attr))
+    return result
+
+
+def function_to_dict(function) -> dict:
+    result = {}
+    for name, value in inspect.getmembers(function):
+        if name == DEFAULTS_FIELD_NAME or name == CLOSURE_FIELD_NAME:
+            result[name] = obj_to_intermediate_format(value)
+            continue
+        if name == CODE_FIELD_NAME:
+            code = value
+            result[CODE_FIELD_NAME] = code_to_dict(code)
+            result[GLOBALS_FIELD_NAME] = {}
+            glob = function.__getattribute__(GLOBALS_FIELD_NAME)
+            for co_name in code.__getattribute__(CODE_NAMES_FIELD_NAME):
+                if co_name == function.__name__:
+                    result[GLOBALS_FIELD_NAME][co_name] = function.__name__
+                    continue
+                if co_name in glob and not inspect.ismodule(glob[co_name]) and not inspect.isbuiltin(glob[co_name]):
+                    print(f"Debug: {glob[co_name]}")
+                    result[GLOBALS_FIELD_NAME][co_name] = obj_to_intermediate_format(glob[co_name])
+
+    return result
+
+
+def class_to_dict(cls) -> dict:
+    pass
+
+
+def obj_to_intermediate_format(obj) -> dict or int or float or complex or bool or str or bytes or None:
+    result = {}
+    if isinstance(obj, (int, float, complex, bool, str, bytes)) or obj is None:
+        return obj
+    if isinstance(obj, (list, tuple)):
+        return sequence_to_dict(obj)
+    if isinstance(obj, dict):
+        return dict_to_dict(obj)
+    if inspect.isfunction(obj) or inspect.ismethod(obj):
+        return function_to_dict(obj)
+    if inspect.isclass(obj):
+        pass
+    else:
+        pass
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+import inspect
+from types import *
+import Serializer
+
+import math
+c = 42
+
+
+def f(x):
+    a = 123
+    return math.sin(x * a * c)
+
+
 
 
 def main() -> None:
-    pass
+    global a
+    a = 3
+    t = (1, 5, 7, 10)
+    l = [1, 2, t, 4, 5]
+    print(Serializer.obj_to_intermediate_format(f))
+
+
+    print("--------------------------------------------------------")
+    # for attr in CODE_ATTRIBUTES:
+    #     print(f"Attr: {attr}: Type: {type(main.__code__.__getattribute__(attr))}, value: {main.__code__.__getattribute__(attr)}")
+
+    # for key, value in main.__globals__.items():
+    #     print(inspect.ismodule(key))
+    #print(inspect.ismodule(main.__globals__["__loader__"]))
+
+
 
 
 if __name__ == '__main__':
