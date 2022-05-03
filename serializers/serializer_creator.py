4feba4e34c6039da00f2e37cd1ff69aca80eb133
Matvey-Makaro <matvey.makaro@gmail.com>
2022-05-03 17:58:55 +0300

    Add docstrings to serializer_creator.

--- serializers/serializer_creator.py
+++ serializers/serializer_creator.py
@@ -1,10 +1,13 @@
+"""The module that provides class SerializerCreator to create serializers of different formats."""
 from serializers.serializer import Serializer
 from serializers.json_serializer import JsonSerializer
-from toml_serializer import TomlSerializer
-from yaml_serializer import YamlSerializer
+from serializers.toml_serializer import TomlSerializer
+from serializers.yaml_serializer import YamlSerializer
 
 
 class SerializerCreator:
+    """Class with main method create to create serializers of different formats."""
+    @staticmethod
     def create(serializer_name: str) -> Serializer:
         """Returns particular serializer with received serializer name. (Possible : Json, Yaml, Toml)."""
         serializer_name = serializer_name.lower()
