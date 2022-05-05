87d6e743e725a6e2b84abe6b40ecf6a54b967ac3
Matvey-Makaro <matvey.makaro@gmail.com>
2022-05-05 13:37:13 +0300

    Fix toml serialization and deserialization.

--- serializers/toml_serializer.py
+++ serializers/toml_serializer.py
@@ -15,6 +15,7 @@ class TomlSerializer(Serializer):
     Class that allows to convert objects to toml format and vice versa.
     Provides four main methods: dumps, dump, loads, load.
     """
+
     @staticmethod
     def dumps(obj: Any) -> str:
         """
@@ -23,6 +24,7 @@ class TomlSerializer(Serializer):
         :return: str
         """
         form = IntermediateFormatSerializer.obj_to_intermediate_format(obj)
+        form = TomlSerializer._prepare_to_serialize(form)
         return tomli_w.dumps(form)
 
     @staticmethod
@@ -34,6 +36,7 @@ class TomlSerializer(Serializer):
         :return: None
         """
         form = IntermediateFormatSerializer.obj_to_intermediate_format(obj)
+        form = TomlSerializer._prepare_to_serialize(form)
         with open(file_name, 'w') as f:
             f.write(tomli_w.dumps(form))
 
@@ -45,6 +48,7 @@ class TomlSerializer(Serializer):
         :return: Any
         """
         form = tomli.loads(serialized_obj)
+        form = TomlSerializer._prepare_to_deserialize(form)
         return IntermediateFormatSerializer.intermediate_format_to_obj(form)
 
     @staticmethod
@@ -58,5 +62,43 @@ class TomlSerializer(Serializer):
         with open(file_name, "r") as f:
             for line in f:
                 ser_obj += line
-            form = tomli.loads(ser_obj)
+        form = tomli.loads(ser_obj)
+        form = TomlSerializer._prepare_to_deserialize(form)
         return IntermediateFormatSerializer.intermediate_format_to_obj(form)
+
+    @staticmethod
+    def _prepare_to_serialize(obj: dict) -> dict:
+        """
+        Prepares an intermediate format for serialization in toml format
+        Replace Null to string "__null__".
+        :param obj: dict
+        :return: dict
+        """
+        for key, value in obj.items():
+            if type(value) == dict:
+                TomlSerializer._prepare_to_serialize(value)
+            elif type(value) == list:
+                for i in range(len(value)):
+                    if value[i] is None:
+                        obj[key][i] = "__none__"
+            elif value is None:
+                obj[key] = "__none__"
+        return obj
+
+    @staticmethod
+    def _prepare_to_deserialize(obj: dict) -> dict:
+        """
+        Replace string "__null__" to Null.
+        :param obj: dict
+        :return: dict
+        """
+        for key, value in obj.items():
+            if type(value) == dict:
+                obj[key] = TomlSerializer._prepare_to_deserialize(value)
+            elif type(value) == list:
+                for i in range(len(value)):
+                    if value[i] == "__none__":
+                        obj[key][i] = None
+            elif value == "__none__":
+                obj[key] = None
+        return obj
